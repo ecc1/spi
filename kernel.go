2b1524b0bad6118306378599ad627aa8f3040d70
Eric Cooper <ecc@cmu.edu>
2016-04-18 18:05:58 -0400

    Initial version

---
+++ kernel.go
@@ -0,0 +1,66 @@
+package spi
+
+// Definitions from <linux/spi/spidev.h>
+
+type spi_ioc_transfer struct {
+	tx_buf uint64
+	rx_buf uint64
+
+	len      uint32
+	speed_hz uint32
+
+	delay_usecs   uint16
+	bits_per_word uint8
+	cs_change     uint8
+	tx_nbits      uint8
+	rx_nbits      uint8
+	pad           uint16
+}
+
+const (
+	spi_CPHA = 0x01
+	spi_CPOL = 0x02
+
+	spi_MODE_0 = (0 | 0)
+	spi_MODE_1 = (0 | spi_CPHA)
+	spi_MODE_2 = (spi_CPOL | 0)
+	spi_MODE_3 = (spi_CPOL | spi_CPHA)
+
+	spi_CS_HIGH   = 0x04
+	spi_LSB_FIRST = 0x08
+	spi_3WIRE     = 0x10
+	spi_LOOP      = 0x20
+	spi_NO_CS     = 0x40
+	spi_READY     = 0x80
+	spi_TX_DUAL   = 0x100
+	spi_TX_QUAD   = 0x200
+	spi_RX_DUAL   = 0x400
+	spi_RX_QUAD   = 0x800
+
+	spi_IOC_MESSAGE_base = 0x40006B00
+	spi_IOC_MESSAGE_incr = 0x200000
+
+	// Read / Write of SPI mode (spi_MODE_0..spi_MODE_3) (limited to 8 bits)
+	spi_IOC_RD_MODE = 0x80016B01
+	spi_IOC_WR_MODE = 0x40016B01
+
+	// Read / Write SPI bit justification
+	spi_IOC_RD_LSB_FIRST = 0x80016B02
+	spi_IOC_WR_LSB_FIRST = 0x40016B02
+
+	// Read / Write SPI device word length (1..N)
+	spi_IOC_RD_BITS_PER_WORD = 0x80016B03
+	spi_IOC_WR_BITS_PER_WORD = 0x40016B03
+
+	// Read / Write SPI device default max speed Hz
+	spi_IOC_RD_MAX_SPEED_HZ = 0x80046B04
+	spi_IOC_WR_MAX_SPEED_HZ = 0x40046B04
+
+	// Read / Write of the SPI mode field
+	spi_IOC_RD_MODE32 = 0x80046B05
+	spi_IOC_WR_MODE32 = 0x40046B05
+)
+
+func spi_IOC_MESSAGE(n uint) uint {
+	return spi_IOC_MESSAGE_base + n*spi_IOC_MESSAGE_incr
+}
---
+++ spi.go
@@ -0,0 +1,163 @@
+package spi
+
+import (
+	"fmt"
+	s "syscall"
+	"unsafe"
+
+	"github.com/ecc1/gpio"
+)
+
+type Device struct {
+	fd    int
+	speed int
+	cs    gpio.OutputPin
+}
+
+const (
+	// FIX THIS to support platforms other than Intel Edison.
+	spiDevice = "/dev/spidev5.1"
+)
+
+func Open(speed int, customCSn int) (*Device, error) {
+	fd, err := s.Open(spiDevice, s.O_RDWR, 0)
+	if err != nil {
+		return nil, err
+	}
+	dev := &Device{fd: fd, speed: speed}
+	if customCSn != 0 {
+		dev.cs, err = gpio.Output(customCSn, true)
+	}
+	return dev, err
+}
+
+func (dev *Device) Close() error {
+	return s.Close(dev.fd)
+}
+
+// Write writes len(buf) bytes from buf to dev.
+func (dev *Device) Write(buf []byte) error {
+	if dev.cs != nil {
+		dev.cs.Write(true)
+		defer dev.cs.Write(false)
+	}
+	n, err := s.Write(dev.fd, buf)
+	if err != nil {
+		return err
+	}
+	if n != len(buf) {
+		return fmt.Errorf("wrote %d bytes instead of %d", n, len(buf))
+	}
+	return nil
+}
+
+// Read reads from dev into buf, blocking if necessary
+// until exactly len(buf) bytes have been read.
+func (dev *Device) Read(buf []byte) error {
+	if dev.cs != nil {
+		dev.cs.Write(true)
+		defer dev.cs.Write(false)
+	}
+	for off := 0; off < len(buf); {
+		n, err := s.Read(dev.fd, buf[off:])
+		if err != nil {
+			return err
+		}
+		off += n
+	}
+	return nil
+}
+
+func (dev *Device) Transfer(buf []byte) error {
+	if dev.cs != nil {
+		dev.cs.Write(true)
+		defer dev.cs.Write(false)
+	}
+	bufAddr := uint64(uintptr(unsafe.Pointer(&buf[0])))
+	tr := spi_ioc_transfer{
+		tx_buf:        bufAddr,
+		rx_buf:        bufAddr,
+		len:           uint32(len(buf)),
+		speed_hz:      uint32(dev.speed),
+		delay_usecs:   1,
+		bits_per_word: 8,
+	}
+	_, _, errno := s.Syscall(s.SYS_IOCTL, uintptr(dev.fd),
+		uintptr(spi_IOC_MESSAGE(1)), uintptr(unsafe.Pointer(&tr)))
+	if errno != 0 {
+		return error(errno)
+	}
+	return nil
+}
+
+func (dev *Device) Mode() (mode int, err error) {
+	_, _, errno := s.Syscall(s.SYS_IOCTL, uintptr(dev.fd),
+		uintptr(spi_IOC_RD_MODE), uintptr(unsafe.Pointer(&mode)))
+	if errno != 0 {
+		err = error(errno)
+	}
+	return
+}
+
+func (dev *Device) SetMode(mode int) error {
+	_, _, errno := s.Syscall(s.SYS_IOCTL, uintptr(dev.fd),
+		uintptr(spi_IOC_WR_MODE), uintptr(unsafe.Pointer(&mode)))
+	if errno != 0 {
+		return error(errno)
+	}
+	return nil
+}
+
+func (dev *Device) LsbFirst() (lsb bool, err error) {
+	_, _, errno := s.Syscall(s.SYS_IOCTL, uintptr(dev.fd),
+		uintptr(spi_IOC_RD_LSB_FIRST), uintptr(unsafe.Pointer(&lsb)))
+	if errno != 0 {
+		err = error(errno)
+	}
+	return
+}
+
+func (dev *Device) SetLsbFirst(lsb bool) error {
+	_, _, errno := s.Syscall(s.SYS_IOCTL, uintptr(dev.fd),
+		uintptr(spi_IOC_WR_LSB_FIRST), uintptr(unsafe.Pointer(&lsb)))
+	if errno != 0 {
+		return error(errno)
+	}
+	return nil
+}
+
+func (dev *Device) BitsPerWord() (bits int, err error) {
+	_, _, errno := s.Syscall(s.SYS_IOCTL, uintptr(dev.fd),
+		uintptr(spi_IOC_RD_BITS_PER_WORD), uintptr(unsafe.Pointer(&bits)))
+	if errno != 0 {
+		err = error(errno)
+	}
+	return
+}
+
+func (dev *Device) SetBitsPerWord(bits int) error {
+	_, _, errno := s.Syscall(s.SYS_IOCTL, uintptr(dev.fd),
+		uintptr(spi_IOC_WR_BITS_PER_WORD), uintptr(unsafe.Pointer(&bits)))
+	if errno != 0 {
+		return error(errno)
+	}
+	return nil
+}
+
+func (dev *Device) MaxSpeed() (speed int, err error) {
+	_, _, errno := s.Syscall(s.SYS_IOCTL, uintptr(dev.fd),
+		uintptr(spi_IOC_RD_MAX_SPEED_HZ), uintptr(unsafe.Pointer(&speed)))
+	if errno != 0 {
+		err = error(errno)
+	}
+	return
+}
+
+func (dev *Device) SetMaxSpeed(speed int) error {
+	_, _, errno := s.Syscall(s.SYS_IOCTL, uintptr(dev.fd),
+		uintptr(spi_IOC_WR_MAX_SPEED_HZ), uintptr(unsafe.Pointer(&speed)))
+	if errno != 0 {
+		return error(errno)
+	}
+	return nil
+}
