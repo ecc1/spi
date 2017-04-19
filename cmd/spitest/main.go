e8dde718c630ee1109b3d8ea2985585da8d1092b
Eric Cooper <ecc@cmu.edu>
2017-04-19 13:57:43 -0400

    Add SPI test command

---
+++ cmd/spitest/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/ecc1/spi"
+)
+
+var (
+	device = flag.String("d", "/dev/spidev5.1", "SPI `device`")
+	speed  = flag.Int("s", 1000000, "SPI `speed` (Hz)")
+)
+
+func main() {
+	flag.Parse()
+	var values []byte
+	for _, v := range flag.Args() {
+		b, err := strconv.ParseUint(v, 16, 8)
+		if err != nil {
+			log.Fatal(err)
+		}
+		values = append(values, byte(b))
+	}
+	dev, err := spi.Open(*device, *speed)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dev.Close()
+	if len(values)%2 == 1 {
+		values = append(values, 0)
+	}
+	fmt.Printf("send: % X\n", values)
+	err = dev.Transfer(values)
+	if err != nil {
+		log.Fatalf("%s: %v", *device, err)
+	}
+	fmt.Printf("recv: % X\n", values)
+}
